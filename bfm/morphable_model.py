0a44b00401c0904e62bc2b16d51a318bfcde55b7
kimoktm <cg.karimtarek@gmail.com>
2019-01-04 18:54:50 +0100

    add submodules

---
+++ bfm/morphable_model.py
@@ -0,0 +1,107 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+from . import load
+# from .. import mesh
+
+class  MorphabelModel(object):
+    """docstring for  MorphabelModel
+    model: nver: number of vertices. ntri: number of triangles. *: must have. ~: can generate ones array for place holder.
+            'shapeMU': [3*nver, 1]. *
+            'shapePC': [3*nver, n_shape_para]. *
+            'shapeEV': [n_shape_para, 1]. ~
+            'expMU': [3*nver, 1]. ~ 
+            'expPC': [3*nver, n_exp_para]. ~
+            'expEV': [n_exp_para, 1]. ~
+            'texMU': [3*nver, 1]. ~
+            'texPC': [3*nver, n_tex_para]. ~
+            'texEV': [n_tex_para, 1]. ~
+            'tri': [ntri, 3] (start from 1, should sub 1 in python and c++). *
+            'tri_mouth': [114, 3] (start from 1, as a supplement to mouth triangles). ~
+            'kpt_ind': [68,] (start from 1). ~
+    """
+    def __init__(self, model_path, model_type = 'BFM'):
+        super( MorphabelModel, self).__init__()
+        if model_type=='BFM':
+            self.model = load.load_BFM(model_path)
+        else:
+            print('sorry, not support other 3DMM model now')
+            exit()
+            
+        # fixed attributes
+        self.nver = self.model['shapePC'].shape[0]/3
+        self.ntri = self.model['tri'].shape[0]
+        self.n_shape_para = self.model['shapePC'].shape[1]
+        self.n_exp_para = self.model['expPC'].shape[1]
+        self.n_tex_para = self.model['texPC'].shape[1]
+        self.triangles = self.model['tri']
+
+        # limit PCA params
+        # self.n_shape_para = 10
+        # self.n_tex_para = 10
+
+
+    # ------------------------------------- shape: represented with mesh(vertices & triangles(fixed))
+    def get_shape_para(self, type = 'random', std = 1.2):
+        if type == 'zero':
+            sp = np.zeros((self.n_shape_para, 1))
+        elif type == 'random':
+            sp = np.random.uniform(-std, std, [self.n_shape_para, 1])
+
+        return sp
+
+    def get_exp_para(self, type = 'random', std = 1.2):
+        if type == 'zero':
+            ep = np.zeros((self.n_exp_para, 1))
+        elif type == 'random':
+            ep = np.random.uniform(-std, std, [self.n_exp_para, 1])
+
+        return ep 
+
+
+    def get_tex_para(self, type = 'random', std = 1.2):
+        if type == 'zero':
+            tx = np.zeros((self.n_tex_para, 1))
+        elif type == 'random':
+            tx = np.random.uniform(-std, std, [self.n_tex_para, 1])
+
+        return tx 
+
+    def generate_vertices(self, shape_para, exp_para):
+        '''
+        Args:
+            shape_para: (n_shape_para, 1)
+            exp_para: (n_exp_para, 1) 
+        Returns:
+            vertices: (nver, 3)
+        '''
+
+        # shape_para = tf.expand_dims(shape_para, 1)
+        exp_para = tf.expand_dims(exp_para, 1)
+
+        # multiply by Std to have normalized params
+        shape_para = shape_para * self.model['shapeEV'][:self.n_shape_para]
+        exp_para = exp_para * self.model['expEV'][:self.n_exp_para]
+        shape_influence = tf.tensordot(self.model['shapePC'][:, :self.n_shape_para], shape_para, 1)
+        exp_influence = tf.tensordot(self.model['expPC'][:, :self.n_exp_para], exp_para, 1)
+        vertices = self.model['shapeMU'] + shape_influence + exp_influence
+        vertices = tf.reshape(vertices, [int(self.nver), 3])
+
+        return vertices
+
+
+    def generate_colors(self, tex_para):
+        '''
+        Args:
+            tex_para: (n_tex_para, 1)
+        Returns:
+            colors: (nver, 3)
+        '''
+
+        colors = self.model['texMU'] + tf.tensordot(self.model['texPC'], tex_para*self.model['texEV'], 1)
+        colors = tf.reshape(colors, [int(self.nver), 3]) / 255.
+        
+        return colors
---
+++ bfm/transform.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+
+
+def tf_deg2rad(deg):
+    pi_on_180 = 0.017453292519943295
+
+    return deg * pi_on_180
+
+
+def angle2matrix(angles):
+    ''' get rotation matrix from three rotation angles(degree). right-handed.
+    Args:
+        angles: [3,]. x, y, z angles
+        x: pitch. positive for looking down.
+        y: yaw. positive for looking left. 
+        z: roll. positive for tilting head right. 
+    Returns:
+        R: [3, 3]. rotation matrix.
+    '''
+    # angles = tf.cast(angles, dtype=tf.float32)
+    x, y, z = tf_deg2rad(angles[0]), tf_deg2rad(angles[1]), tf_deg2rad(angles[2])
+
+    # x
+    Rx = tf.stack([1,      0,       0,
+                   0, tf.cos(x),  -tf.sin(x),
+                   0, tf.sin(x),   tf.cos(x)])
+    Rx = tf.reshape(Rx, (3,3))
+
+    # y
+    Ry = tf.stack([tf.cos(y), 0, tf.sin(y),
+                   0,         1,      0,
+                  -tf.sin(y), 0, tf.cos(y)])
+    Ry = tf.reshape(Ry, (3,3))
+
+    # z
+    Rz = tf.stack([tf.cos(z), -tf.sin(z), 0,
+                  tf.sin(z),  tf.cos(z), 0,
+                   0,       0, 1])
+    Rz = tf.reshape(Rz, (3,3))
+
+
+    R = tf.tensordot(Rz, tf.tensordot(Ry, Rx, 1), 1)
+
+    return R
+
+
+def rotate(vertices, angles):
+    ''' rotate vertices. 
+    X_new = R.dot(X). X: 3 x 1   
+    Args:
+        vertices: [nver, 3]. 
+        rx, ry, rz: degree angles
+        rx: pitch. positive for looking down 
+        ry: yaw. positive for looking left
+        rz: roll. positive for tilting head right
+    Returns:
+        rotated vertices: [nver, 3]
+    '''
+
+    # vertices = tf.cast(vertices, dtype=tf.float32)
+
+    R = angle2matrix(angles)
+    rotated_vertices = tf.tensordot(vertices, tf.transpose(R), 1)
+
+    return rotated_vertices
+
+def similarity_transform(vertices, s, angles, t3d):
+    ''' similarity transform. dof = 7.
+    3D: s*R.dot(X) + t
+    Homo: M = [[sR, t],[0^T, 1]].  M.dot(X)
+    Args:(float32)
+        vertices: [nver, 3]. 
+        s: [1,]. scale factor.
+        R: [3,3]. rotation matrix.
+        t3d: [3,]. 3d translation vector.
+    Returns:
+        transformed vertices: [nver, 3]
+    '''
+
+    # vertices = tf.cast(vertices, dtype=tf.float32)
+
+    # TO DO: DELETE
+    s =  8e-04 + s * 1e-05
+
+
+    t3d = tf.squeeze(t3d)
+    transformed_vertices = s * rotate(vertices, angles) + t3d
+
+    return transformed_vertices
